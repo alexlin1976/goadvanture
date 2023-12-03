2621fd4f18badc998dabd2802f87fdf7749720f4
Alex Lin <fishtail.alex.lin@gmail.com>
2023-12-03 17:26:27 +0800

    move villager assets and move the definitions into separate file

--- src/GameScriptLoader.ts
+++ src/GameScriptLoader.ts
@@ -4,24 +4,29 @@ import GameMap from "./GameMap";
 
 class GameScriptLoader {
   script: any;
+  villagers: any;
   constructor() {
   }
 
-  async loadScript(): Promise<void> {
+  async loadJson(path: string): Promise<any> {
     try {
-      const response = await fetch('assets/GameScript.json');
+      const response = await fetch(path);
 
       if (!response.ok) {
         throw new Error(`Network response was not ok: ${response.statusText}`);
       }
 
-      const script: any = await response.json(); // Adjust 'any' based on your JSON structure
-      this.script = script; // Exporting to a global variable
-
-      console.log('Game script loaded:', script);
+      return await response.json(); // Adjust 'any' based on your JSON structure
     } catch (error) {
-      console.error('Error during script loading:', error);
+      console.error(`Error during json ${path} loading:`, error);
     }
+  }
+
+  async loadScript(): Promise<void> {
+    this.script = await this.loadJson('assets/GameScript.json');
+    console.log('Game script loaded:', this.script);
+    this.villagers = await this.loadJson('assets/Villagers.json');
+    console.log('Villagers loaded:', this.script);
   }
 
   startScene() {
@@ -33,7 +38,7 @@ class GameScriptLoader {
   }
 
   villager(key: string): any {
-    return this.script.villagers[key];
+    return this.villagers[key];
   }
 }
   
